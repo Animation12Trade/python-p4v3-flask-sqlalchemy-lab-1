59833ba5b96cf3de57353e3686d98d5e62a95764
+ <omuhindaaaron@gmail.com>
2024-06-30 15:52:40 +0300

    feat: first lab grade.

--- server/app.py
+++ server/app.py
@@ -20,8 +20,29 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+
+@app.route('/earthquakes/<int:id>')
+def earthquakes_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()  # Fix indentation
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all() 
+    body = {'count': len(earthquakes),
+            'quakes': [earthquake.to_dict() for earthquake in earthquakes]}
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
